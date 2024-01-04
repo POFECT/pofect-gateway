e1516393fbe125253b517e85e45e4f849c1ad44b
bongdaring <chlwlsdud258@gmail.com>
2024-01-04 14:27:01 +0900

    first commit

---
+++ src/main/java/com/example/pofectgateway/PofectGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.pofectgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofectGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PofectGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pofectgateway/PofectGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pofectgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofectGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
